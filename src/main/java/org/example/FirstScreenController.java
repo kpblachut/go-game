f2c4748028d05f4684c09642e2723f811793ed32
Kaneki2002g <gabor.salez@gmail.com>
2023-12-18 20:50:37 +0100

    StartMenu somewhat works, methods still not implemented

---
+++ src/main/java/org/example/FirstScreenController.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class FirstScreenController {
+
+    @FXML
+    private Button joinButton;
+
+    @FXML
+    private TextField nicknameField;
+
+    //Metoda po przycisnieciu przycisku
+    @FXML
+    void JoinWIthTheNickname(ActionEvent event) {
+
+    }
+
+}

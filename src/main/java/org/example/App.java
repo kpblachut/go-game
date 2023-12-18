9c957d37ebe992facdad6f2e81f3d4cae87fe2e0
Kaneki2002g <gabor.salez@gmail.com>
2023-12-18 20:49:53 +0100

    StartMenu somewhat works

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,27 @@
 package org.example;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/FirstScreen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),500,500);
+        stage.setTitle("Go Game");
+        stage.setScene(scene);
+        stage.show();
     }
 }

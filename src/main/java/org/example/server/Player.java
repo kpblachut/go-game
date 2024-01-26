d4dcb847859a1efbbf9b6c08242d7fa9bd6f7b9b
kpblachut <kpblachut@gmail.com>
2024-01-26 21:49:35 +0100

    add functionality to the intersection and gameRecord and GameTurn

---
+++ src/main/java/org/example/server/Player.java
@@ -0,0 +1,41 @@
+package org.example.server;
+
+import java.util.Collections;
+
+public class Player {
+    private final int id;
+    private int capturedStones;
+
+    public Player(int id) {
+        this.id = id;
+        this.capturedStones = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCapturedStones() {
+        return capturedStones;
+    }
+
+    public void addCapturedStones(int n) {
+        capturedStones += n;
+    }
+
+    public void removeCapturedStones(int n) {
+        capturedStones -= n;
+    }
+
+    public boolean play(GameBoard gameBoard, int x, int y) {
+        if (x == -1 && y == -1) {
+            GameRecord gameRecord = gameBoard.getGameRecord();
+            gameRecord.apply(gameRecord.getLastTurn().toNext(-1, -1, this.getId(), Collections.<Intersection>emptySet()));
+            gameBoard.updatePassCount(true);
+
+            return true;
+        } else {
+            return gameBoard
+        }
+    }
+}

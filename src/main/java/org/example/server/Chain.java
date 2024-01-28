7754da0e92d0e58c17c7d003f618ed0b871bd251
kpblachut <kpblachut@gmail.com>
2024-01-28 17:37:41 +0100

    complited game logic

--- src/main/java/org/example/server/Chain.java
+++ src/main/java/org/example/server/Chain.java
@@ -1,4 +1,67 @@
 package org.example.server;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Chain {
+    private final Set<Intersection> stones;
+    private final Set<Intersection> liberties;
+    private final Player owner;
+
+    public Chain(Set<Intersection> stones, Set<Intersection> liberties, Player owner) {
+        this.stones = stones;
+        this.liberties = liberties;
+        this.owner = owner;
+    }
+
+    public Chain(Intersection intersection, Player owner) {
+        stones = new HashSet<Intersection>();
+        stones.add(intersection);
+        this.owner = owner;
+        liberties = new HashSet<Intersection>(intersection.getEmptyNeighbors());
+    }
+
+    public Chain(Chain chain) {
+        this.stones = new HashSet<Intersection>(chain.stones);
+        this.liberties = new HashSet<Intersection>(chain.liberties);
+        this.owner = chain.owner;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public Set<Intersection> getLiberties() {
+        return liberties;
+    }
+
+    public Set<Intersection> getStones() {
+        return stones;
+    }
+
+    public void add(Chain chain, Intersection playedStone) {
+        // Fuse stone sets
+        this.stones.addAll(chain.stones);
+        // Fuse liberties
+        this.liberties.addAll(chain.liberties);
+        // remove played stone from liberties
+        this.liberties.remove(playedStone);
+    }
+
+    public Chain removeLiberty(Intersection playedStone) {
+        Chain newChain = new Chain(stones, liberties, owner);
+        newChain.liberties.remove(playedStone);
+        return newChain;
+    }
+
+    public void die() {
+        for (Intersection rollingStone : this.stones) {
+            rollingStone.setChain(null);
+            Set<Chain> neighbouringChains = rollingStone.getNeighbours();
+            for (Chain chain : neighbouringChains) {
+                chain.liberties.add(rollingStone);
+            }
+        }
+        this.owner.addCapturedStones(this.stones.size());
+    }
 }
--- src/main/java/org/example/server/GameBoard.java
+++ src/main/java/org/example/server/GameBoard.java
@@ -1,5 +1,9 @@
 package org.example.server;
 
+import org.example.server.exceptions.OutOfGameBoardException;
+import org.example.server.exceptions.InvalidGameTurnEncounteredException;
+
+import java.security.InvalidParameterException;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -51,10 +55,6 @@ public class GameBoard {
         }
     }
 
-    public void takeGameTurn(GameTurn gameTurn, Player P1, Player P2) {
-
-    }
-
     public int getPassCount() {
         return passCount;
     }
@@ -89,11 +89,172 @@ public class GameBoard {
         updatePassCount(true);
     }
 
-    public boolean play(Intersection intersection, Player player)
+    public boolean isInBoard(int x, int y) {
+        return (x >= 0 && x < width && y >= 0 && y < height);
+    }
 
-    public void nextPlayer() {
+    public boolean isInBoard(Intersection intersection) {
+        int x = intersection.getX();
+        int y = intersection.getY();
+        return (x >= 0 && x < width && y >= 0 && y < height);
+    }
+
+    public Intersection getIntersection(int x, int y) {
+        if (isInBoard(x, y)) {
+            return intersections[x][y];
+        } else {
+            return null;
+        }
+    }
+
+
+    public boolean play(Intersection intersection, Player player, boolean handleKo) {
+        boolean ko = false;
+        GameTurn currentTurn = null;
+
+        if (!isInBoard(intersection)) return false;
+
+        if (intersection.getChain() != null) return false;
+
+        Set<Intersection> capturedStones = null;
+        Set<Chain> capturedChains = null;
+
+        if (handleKo) {
+            capturedStones = new HashSet<Intersection>();
+            capturedChains = new HashSet<Chain>();
+        }
+
+        Set<Chain> neighbouringChains = intersection.getNeighbours();
+        Chain newChain = new Chain(intersection, player);
+        intersection.setChain(newChain);
+        for (Chain chain : neighbouringChains) {
+            if (chain.getOwner() == player) {
+                newChain.add(chain, intersection);
+            } else {
+                chain.removeLiberty(intersection);
+                if (chain.getLiberties().size() == 0) {
+                    if(handleKo) {
+                        capturedStones.addAll(chain.getStones());
+                        capturedChains.add(new Chain(chain));
+                    }
+                    chain.die();
+                }
+            }
+        }
+        if (handleKo) {
+            currentTurn = gameRecord.getLastTurn().toNext(intersection.getX(),intersection.getY(),player.getId(),capturedStones);
+            for (GameTurn turn : gameRecord.getTurns()) {
+                if (turn.equals(currentTurn)) {
+                    ko = true;
+                    break;
+                }
+            }
+            if (ko) {
+                for (Chain chain : capturedChains) {
+                    chain.getOwner().removeCapturedStones(chain.getStones().size());
+                    for (Intersection stone : chain.getStones()) {
+                        stone.setChain(chain);
+                    }
+                }
+            }
+        }
+
+        // Preventing suicide or ko and re-adding liberty
+        if (newChain.getLiberties().size() == 0 || ko) {
+            for (Chain chain : intersection.getNeighbours()) {
+                chain.getLiberties().add(intersection);
+            }
+            intersection.setChain(null);
+            return false;
+        }
+
+        // Move is valid, applying changes
+        for (Intersection stone : newChain.getStones()) {
+            stone.setChain(newChain);
+        }
+        if (handleKo) {
+            gameRecord.apply(currentTurn);
+        }
+
+        lastCaptured = capturedStones;
+        updatePassCount(false);
+        return true;
+    }
+
+    public boolean play(Intersection intersection, Player player) {
+        return play(intersection,player,true);
+    }
+
+    public boolean play(int x, int y, Player player) throws OutOfGameBoardException {
+        Intersection intersection = getIntersection(x, y);
+        if (intersection == null) {
+            throw new OutOfGameBoardException("Intersection is out of range: x=" + x + " y=" + y);
+        }
+        return play(intersection, player);
+    }
+
+    public void freeIntersections() {
+        for(Intersection[] intersectionColumn : intersections) {
+            for(Intersection intersection : intersectionColumn) {
+                intersection.setChain(null);
+            }
+        }
+    }
+
+    public void takeGameTurn(GameTurn gameTurn,Player P1, Player P2) throws InvalidGameTurnEncounteredException, InvalidParameterException {
+        this.freeIntersections();
+        if(gameTurn == null || P1 == null || P2 == null) throw new InvalidParameterException("None of the Parameters should not be null.");
+        if(P1.getId() != 1 || P2.getId() != 2) throw new InvalidParameterException("Incorrect Players entered. One should have an id == 1 and P2 an id == 2, here P1.id = "+P1.getId()+" and P2.id = "+P2.getId());
+        if(gameTurn.getBoardState().length != width || gameTurn.getBoardState()[0].length != height ) throw new InvalidGameTurnEncounteredException("Incompatible board dimensions between GameBoard and given GameTurn");
+
+        int[][] boardState = gameTurn.getBoardState();
+        for (int x = 0; x < width ; x++) {
+            for (int y = 0; y < height ; y++) {
+                switch (boardState[x][y]) {
+                    case 2:
+                        play(getIntersection(x,y),P2,false);
+                        break;
+                    case 1:
+                        play(getIntersection(x,y),P1,false);
+                        break;
+                    case 0:
+                        //DO NOTHING
+                        break;
+                    default:
+                        throw new InvalidGameTurnEncounteredException("Unexpected intersection state encountered: "+boardState[x][y]);
+                }
+            }
+        }
 
     }
+
+    public Player getPlayer() {
+        return currentPlayer;
+    }
+
+    public Player getPlayer(int p) {
+        return switch (p) {
+            case 1 -> P1;
+            case 2 -> P2;
+            default -> null;
+        };
+    }
+
+    public boolean nextPlayer() {
+        return changePlayer(false);
+    }
+
+
+    public boolean changePlayer(boolean undo) {
+        if (currentPlayer == P1) {
+            currentPlayer = P2;
+            System.out.println("Changing player for P2");
+        } else {
+            currentPlayer = P1;
+            System.out.println("Changing player for P1");
+        }
+        return true;
+        }
 
 }
 
